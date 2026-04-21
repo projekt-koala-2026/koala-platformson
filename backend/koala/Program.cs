ee4673beaab9de820e75dfcaa3b45779eb7f251c
jk-git-work <jakub.kaminski.work@gmail.com>
2026-04-21 14:48:13 +0200

    modified docker-compose / backend now serves static files from a secure mounted location / modified .env

--- backend/koala/Program.cs
+++ backend/koala/Program.cs
@@ -6,29 +6,12 @@ using Scalar.AspNetCore;
 using Microsoft.AspNetCore.Authentication;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
-var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
-var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
-var dbUser = Environment.GetEnvironmentVariable("DB_USER");
-var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
-var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5173")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod()
-                  .AllowCredentials();
-        });
-});
 
 //NOTE: VALIDATORS
 builder.Services.AddValidatorsFromAssemblyContaining<UserCreateValidator>();
@@ -39,6 +22,12 @@ builder.Services.AddValidatorsFromAssemblyContaining<UserLoginValidator>();
 
 builder.Services.AddFluentValidationAutoValidation();
 
+//NOTE: CONECTING TO DB
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
 var connectionString =
     $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword}";
 
@@ -47,21 +36,23 @@ builder.Services.AddDbContextFactory<AppDbContext>(options =>
     options.UseNpgsql(connectionString);
 });
 
+//NOTE: ADDING AUTH
 builder.Services
     .AddAuthentication("AdminPanelKoalaScheme")
     .AddScheme<AuthenticationSchemeOptions, KoalaAuthHandler>("AdminPanelKoalaScheme", options => {});
-
 builder.Services.AddAuthorization();
 
+//NOTE: ADDING SERVICES
 builder.Services.AddTransient<AuthService>();
 builder.Services.AddTransient<UserService>();
-//builder.Services.AddTransient<ValidationService>();
 
+//NOTE: ALL ROUTING IS LOWER CASE
 builder.Services.AddRouting(options =>
 {
     options.LowercaseUrls = true;
 });
 
+//NOTE: ADD CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
@@ -77,8 +68,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 app.UseCors("AllowFrontend");
 
-app.UseCors("AllowFrontend");
-
+//NOTE: DELEVOPMENT MODE - ADDS OPENAPI + DB CONFIGS
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -91,6 +81,19 @@ if (app.Environment.IsDevelopment())
         await authService.InicializeDB();
     }
 }
+
+//NOTE: SERVING STATIC FILES (IMAGES & TEXT FILES & PDFS)
+var publicPath = Environment.GetEnvironmentVariable("PUBLIC_STORAGE_PATH");
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(publicPath),
+    RequestPath = "/content",
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+        ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=604800");
+    }
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
