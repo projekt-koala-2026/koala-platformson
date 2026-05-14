c63dee8479a5e99af74bea0cc66035d037fa6bbd
Alicja <aligni@st.amu.edu.pl>
2026-05-14 20:04:14 +0200

    Added Delete User and Edit User`s Roles as Admin at frontend

--- client/src/screens/admin/PanelScreen.jsx
+++ client/src/screens/admin/PanelScreen.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FaEdit, FaTrash } from "react-icons/fa";
 import { ContentsListBox, ContentsListTile } from "../../components/ContentsList";
 import Hamburger from "../../components/Hamburger";
 import MarkdownEditor from "../../components/MarkdownEditor";
@@ -14,6 +15,8 @@ const PanelScreen = () => {
     const [users, setUsers] = useState(null);
     const isAdminEditor = useMemo(() => isAdmin() || isEditor(), []);
     const [posts, setPosts] = useState([]);
+    const [editingUser, setEditingUser] = useState(null);
+    const [selectedRoles, setSelectedRoles] = useState([]);
 
     const handleLogout = async () => {
         const data = await apiRequest("/api/admin/auth/session", {}, "DELETE", navigate);
@@ -26,6 +29,49 @@ const PanelScreen = () => {
 
     const AddUser = async () => {
         navigate("/admin/adduser");
+    };
+
+    const DeleteUser = async (user) => {
+        const confirmed = window.confirm(
+            `Czy na pewno chcesz usunąć użytkownika ${user.email}?`
+        );
+
+        if (!confirmed) return;
+
+        const data = await apiRequest("/api/admin/user/user", { id: user.id}, "DELETE", navigate);
+        setUsers(prev => prev.filter(u => u.id !== user.id));
+    };
+
+    const EditUser = (user) => {
+        setEditingUser(user);
+        setSelectedRoles(user.roles || []);
+    };
+
+    const toggleRole = (role) => {
+        setSelectedRoles(prev =>
+            prev.includes(role)
+                ? prev.filter(r => r !== role)
+                : [...prev, role]
+        );
+    };
+
+    const saveRoles = async () => {
+        await apiRequest(
+            "/api/admin/user/roles",
+            { id: editingUser.id, newRoles: selectedRoles },
+            "PUT",
+            navigate
+        );
+
+        setUsers(prev =>
+            prev.map(u =>
+                u.id === editingUser.id
+                    ? { ...u, roles: selectedRoles }
+                    : u
+            )
+        );
+
+        setEditingUser(null);
     };
 
     const ChangePassword = async () => {
@@ -57,9 +103,40 @@ const PanelScreen = () => {
                     <ContentsListBox>
                         {users.map((item, idx) => (
                             <ContentsListTile key={"users-list" + idx}>
-                                <span>{item.email}</span>
+                                <div style={{ display: "flex"}}>
+                                    <div style={{ display: "flex", flexDirection: "column" }}>
+                                        <span>{item.email}</span>
+                                        <p>Role to: <span style={{textTransform: "lowercase"}}>{item.roles.join(", ")}</span></p>
+                                    </div>
+                                    
+                                    <div style={{display: "flex", gap: "4px", marginLeft: "auto",}}>
+                                        <Button text={<FaEdit />} onClick={() => EditUser(item)} />
+                                        <Button text={<FaTrash />} onClick={() => DeleteUser(item)} />
+                                    </div>
+                                </div>
+                                {editingUser?.id === item.id && (
+                                    <div>
+                                        <h3>Edytuj Rolę dla {editingUser.email}</h3>
+
+                                        {["ADMIN", "EDITOR", "REVIEWER"].map(role => (
+                                            <label key={role} style={{ display: "block" }}>
+                                                <input
+                                                    type="checkbox"
+                                                    checked={selectedRoles.includes(role)}
+                                                    onChange={() => toggleRole(role)}
+                                                />
+                                                {role.toLowerCase()}
+                                            </label>
+                                        ))}
+
+                                        <div style={{ display: "flex", gap: "10px", marginTop: "10px" }}>
+                                            <Button text={"Zapisz"} onClick={saveRoles} />
+                                            <Button text={"Anuluj"} onClick={() => setEditingUser(null)} />
+                                        </div>
+                                    </div>
+                                )}
                             </ContentsListTile>
-                        ))}
+                        ))}          
                     </ContentsListBox>
                     <Button text={"Dodaj nowego użytkownika"} onClick={AddUser} />
                 </>
