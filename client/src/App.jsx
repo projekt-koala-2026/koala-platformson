73705ca19fcc0a114a390e519f87c2ba8343cb75
unknown <aligni@st.amu.edu.pl>
2026-06-02 17:27:39 +0200

    Add History Admin site and fixed Rules Admin site

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import EditionsScreen from "./screens/admin/EditEditionScreen.jsx";
 import EditKoalicjantInfo from "./screens/admin/EditKoalicjantInfoScreen.jsx";
 import EditPost from "./screens/admin/EditPostScreen.jsx";
 import EditRule from "./screens/admin/EditRuleScreen.jsx";
+import EditHistory from "./screens/admin/EditHistoryScreen.jsx";
 import EditSponsorInfo from "./screens/admin/EditSponsorInfoScreen.jsx";
 import ImageHandlingScreen from "./screens/admin/ImageHandlingScreen";
 import LoginScreen from "./screens/admin/LoginScreen";
@@ -27,6 +28,7 @@ export default function App() {
                     <Route path="/admin/sponsors" element={<EditSponsorInfo />} />
                     <Route path="/admin/koalicjants" element={<EditKoalicjantInfo />} />
                     <Route path="/admin/rules" element={<EditRule />} />
+                    <Route path="/admin/history" element={<EditHistory />} />
                     <Route path="/admin/posts" element={<EditPost />} />
                     <Route path="/admin/editions" element={<EditionsScreen />} />
                     <Route path="/" element={<HomeScreen />} />
