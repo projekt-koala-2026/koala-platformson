2fe9710233c0a7e7a8bf7ec98a13d3a954b1e903
angrypigs <angrypigs.tnt@gmail.com>
2026-09-25 19:19:14 +0200

    migration commit

--- backend/koala/src/Modules/Cms/Data/Migrations/20260925164700_SomePendingChange.Designer.cs
+++ backend/koala/src/Modules/Cms/Data/Migrations/20260925164700_SomePendingChange.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using koala.src.Modules.Cms.Data;
@@ -11,9 +12,11 @@ using koala.src.Modules.Cms.Data;
 namespace koala.src.Modules.Cms.Data.Migrations
 {
     [DbContext(typeof(CmsDbContext))]
-    partial class CmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260925164700_SomePendingChange")]
+    partial class SomePendingChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
